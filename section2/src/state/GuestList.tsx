59774841ad1da3dd597256431f041082c15aadad
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-17 19:39:31 +0200

    impl guestlist

--- section2/src/state/GuestList.tsx
+++ section2/src/state/GuestList.tsx
@@ -2,13 +2,25 @@ import { useState } from "react"
 
 export const GuestList: React.FC = () => {
       const [name, setName] = useState("")
+      const [guest, setGuest] = useState<string[]>([])
+
+
+       const onClick = () => {
+            setName("")
+            setGuest([...guest, name])
+      }
+      
     return (
         <div>
             <h1>Jakob's party</h1>
             <h3>Guest List</h3>
+            <ul>
+                {guest.map(guest => <li key={guest} > {guest} </li>)}
+            </ul>
 
             <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
-            <button>Add Guest</button>
+            <button onClick={onClick} >Add Guest</button>
+            
         </div>
     )
 }
