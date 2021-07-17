1ebaada28f80f847ca3e34e3bfd81a6f171415f8
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-17 18:56:57 +0200

    first commit

---
+++ section2/src/index.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from "react-dom"
+
+
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+
+)
+export default App
