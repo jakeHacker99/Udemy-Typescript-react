9d8bcf1a0573aff626cec038a2b86126cf5f7b45
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-20 18:27:12 +0200

    impl solutoin nested packages

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -15,9 +15,10 @@ export const unpkgPathPlugin = () => {
                 if (args.path.includes("./" || args.path.inclues("../"))) {
                     return {
                         namespace: "a",
-                        path: new URL(args.path, args.importer + "/").href,
+                        path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href,
                     }
                 }
+
                 return {
                     namespace: "a",
                     path: `https://unpkg.com/${args.path}`
@@ -32,15 +33,17 @@ export const unpkgPathPlugin = () => {
                     return {
                         loader: 'jsx',
                         contents: `
-              const message = require("medium-test-pkg")
+              const message = require("nested-test-pkg")
               console.log(message);
             `,
                     };
                 }
-                const { data } = await axios.get(args.path)
+                const { data, request } = await axios.get(args.path)
+
                 return {
                     loader: "jsx",
-                    contents: data
+                    contents: data,
+                    resolveDir: new URL("./", request.responseURL).pathname
                 }
             });
         },
