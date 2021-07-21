2c7a5964956065605144bea86e978cdcd0ebae9c
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-21 13:59:08 +0200

    Tricking ESBuild's CSS Handling

--- jbook/src/plugins/fetch-plugin.ts
+++ jbook/src/plugins/fetch-plugin.ts
@@ -22,15 +22,25 @@ export const fetchPlugin = (inputCode: string) => {
 
                 const cachedResult = await fileCacha.getItem<esbuild.OnLoadResult>(args.path)
 
-                if (cachedResult) {
-                    return cachedResult
-                }
+                // if (cachedResult) {
+                //     return cachedResult
+                // }
+
 
                 const { data, request } = await axios.get(args.path)
 
+                const fileType = args.path.match(/.css$/) ? "css" : "jsx"
+
+                const contents = fileType === "css"
+                    ? `
+                const style = document.createElement("style");
+                style.innerText = 'body {background-color: "blue"}';
+                document.head.appendChild(style)
+                `:
+                    data;
                 const result: esbuild.OnLoadResult = {
                     loader: "jsx",
-                    contents: data,
+                    contents,
                     resolveDir: new URL("./", request.responseURL).pathname
                 }
                 await fileCacha.setItem(args.path, result)
