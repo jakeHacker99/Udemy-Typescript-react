4592e98e7eecb7a3cbd74b3d50d5c8d4ee7f5f91
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-07-25 12:18:24 +0200

    Handling Async Errors

--- jbook/src/components/preview.tsx
+++ jbook/src/components/preview.tsx
@@ -8,20 +8,29 @@ interface PreviewProps {
 const html = `
 <html>
 <head>
-<style>html {background-color: blueviolet</style>
+<style>html {background-color: blueviolet }</style>
 </head>
 <body>
 <div id="root"></div>
 <script>
+ const handleError = (err) => {
+  const root = document.querySelector("#root");
+  root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + "</div>";
+  console.error(err) ;
+  
+}
 
+window.addEventListener("error", (event) => {
+  event.preventDefault();
+  handleError(event.error)
+})
 
 window.addEventListener('message', (event) => {
   try{
     eval(event.data)
   } catch(err) {
-    const root = document.querySelector("#root");
-    root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + "</div>";
-    throw err;
+    handleError(err)
+
   }
 }, false);
 
